da367dee55f2e0cc1d3000d974338a184527bea4
TeeAkinlade <taiye.akinlade!@gmail.com>
2023-12-29 14:00:46 +0100

    t

--- app/auth/callback/route.js
+++ app/auth/callback/route.js
@@ -1,17 +1,17 @@
-import { createRouteHandlerClient} from "@supabase/auth-helpers-nextjs";
-import { cookies } from 'next/headers';
-import  { NextResponse } from 'next/server';
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
 
 export async function GET(request) {
     const requestUrl = new URL(request.url);
     //receive the code
-    const code = requestUrl.searchParams.get('code');
-    if (code){
+    const code = requestUrl.searchParams.get("code");
+    if (code) {
         //create a cookiesStore
         const cookieStore = cookies();
-        const supabase = createRouteHandlerClient({cookies: () => cookieStore})
+        const supabase = createRouteHandlerClient({ cookies: () => cookieStore });
         //exchange the code recieved and start a cookies
         await supabase.auth.exchangeCodeForSession(code);
     }
-    return NextResponse.redirect(requestUrl.origin)
+    return NextResponse.redirect(requestUrl.origin);
 }
