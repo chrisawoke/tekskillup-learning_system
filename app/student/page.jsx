bb4ac04fe4d2ca756dd998827f8875092c298474
TeeAkinlade <taiye.akinlade!@gmail.com>
2023-12-29 15:06:01 +0100

    third commit

--- app/student/page.jsx
+++ app/student/page.jsx
@@ -33,7 +33,7 @@ const Homepage = () => {
       <h3>Welcome back, {user?.user_metadata?.first_name}</h3>
       <div className="h-screen flex flex-col justify-center items-center bg-gray-100">
                 <div className="bg-white dark:bg-gray-900 p-8 rounded-lg shadow-md w-98 text-center">
-                    <h1 className="mb-4 text-4xl font-bold text-gray-900 dark:text-gray-300"> You're already logged in</h1>
+                    <h1 className="mb-4 text-4xl font-bold text-gray-900 dark:text-gray-300"> You are already logged in</h1>
                     <button 
                         className="bg-red-400 w-full rounded-lg p-3 text-white hover:bg-red-600"
                         onClick={handleLogout}>LogOut</button>
