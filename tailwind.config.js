2fe733be0c3511e75a99bcf92053d7a1e11378ad
Chris Awoke <uchechristian27@gmail.com>
2024-01-05 08:58:56 +0100

    Updated the global.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,37 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: { screens: {
-    xs: "360px",
-    sm: "480px",
-    md: "768px",
-    lg: "976px",
-    xl: "1440px",
-  },
+  theme: {
+    screens: {
+      xs: "360px",
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      screens: {
+        xs: "360px",
+        sm: "480px",
+        md: "768px",
+        lg: "976px",
+        "2lg": "1160px",
+        xl: "1440px",
       },
       fontFamily: {
-        inter: ['Inter', "sans-serif"],
+        inter: ["Inter", "sans-serif"],
       },
       colors: {
-       primaryColor: '#5429FF',
-       accentColor: '#CBCCE8',
-       gray: '#344054',
-       lightGray: '#667085',
+        primaryColor: "#5429FF",
+        accentColor: "#CBCCE8",
+        gray: "#344054",
+        lightGray: "#667085",
       },
     },
   },
   plugins: [],
-}
+};
