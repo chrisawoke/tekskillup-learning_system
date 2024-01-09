d1be10aebbe8678410b1988125a033a3444be75c
Chris Awoke <uchechristian27@gmail.com>
2024-01-09 16:57:05 +0100

    Update Hero.jsx

--- app/contact/Hero.jsx
+++ app/contact/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = () => {
         className="md:min-h-screen py-10 px-5 flex items-center justify-center md:bg-cover"
       >
         <div className=" text-center text-white px:1 md:px-20">
-          <h2 className="text-2xl font-inter font-bold mb-10 md:text-[40px] md:font-extrabold">
+          <h2 className="text-2xl font-bold mb-10 md:text-[40px] md:font-extrabold">
             Contact Us
           </h2>
           <p className="text-md font-light md:text-[20px]">{contactMessage}</p>
