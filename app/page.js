c09e95c2aae9f5c58a7d90781423d794cc107d20
Chris Awoke <uchechristian27@gmail.com>
2024-01-09 11:57:48 +0100

    Update

--- app/page.js
+++ app/page.js
@@ -1,10 +1,5 @@
 import Image from "next/image";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="hero__title">Get Started!</h1>
-      <button className="custom-btn">Click it</button>
-    </main>
-  );
+  return <main className=""></main>;
 }
