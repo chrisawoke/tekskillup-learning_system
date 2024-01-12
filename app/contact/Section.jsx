a15c8983d41d391abf21f1f283e5d4bde5960272
Ifeoluwa <soyomboifeoluwa90@gmail.com>
2024-01-13 00:09:10 +0100

    contact-revamp

--- app/contact/Section.jsx
+++ app/contact/Section.jsx
@@ -2,14 +2,16 @@ import { IoLocationSharp } from "react-icons/io5";
 import { IoMdMail } from "react-icons/io";
 import { FaPhoneAlt } from "react-icons/fa";
 import Form from "./Form";
+import Image from 'next/image';
+
 
 const ContactSection = () =>{
     return(
         <main>
             <div className="md:flex justify-end items-center md:py-5 hidden md:w-[100%] h-[100%]">
-                <img src="/assets/image/Frame 427320786.png" alt=""/>
+                <Image src="/assets/image/Frame 427320786.png" alt="Section"  width={400} height={300}/>
             </div>
-        <div className="font-inter px-10 py-5 mt-10 md:flex justify-center items-center w-[100%] gap-20 md:px-20 md:py-10 md:mt-[-250px]">
+        <div className=" px-10 py-5 mt-10 md:flex justify-center items-center w-[100%] gap-[200px] md:px-20 md:py-10 md:mt-[-250px]">
             <div className="md:w-[50%]">
                 <Form/>
                 </div>
@@ -29,17 +31,17 @@ const ContactSection = () =>{
                          odio justo viverra odio, eu feugiat nisl.
                     </p>
                     <div className="mt-10">
-                        <div className="mb-7 flex justify-start items-start">
-                            <div className="border border-solid border-gray-500 p-2 rounded cursor-pointer"> <IoMdMail style={{ color: 'rgba(10, 55, 47, 1)'}} className="h-5 w-5"/></div>
-                            <p className="font-normal ml-3 mt-1">support@skillbridge.com</p>
+                        <div className="mb-7 flex justify-start items-start cursor-pointer">
+                            <div className="border-group hover:animate-fade-in"> <IoMdMail style={{ color: 'rgba(10, 55, 47, 1)'}} className="h-5 w-5"/></div>
+                            <p className="font-normal ml-3 mt-1 hover:text-lg">support@skillbridge.com</p>
                         </div>
-                        <div className="mb-7 flex justify-start items-start">
-                           <div className="border border-solid border-gray-500 p-2 rounded cursor-pointer"> <FaPhoneAlt style={{ color: 'rgba(14, 151, 123, 1)'}} className="h-5 w-5" /></div>
-                            <p className="font-normal ml-3 mt-1">+91 00000 00000</p>
+                        <div className="mb-7 flex justify-start items-start cursor-pointer">
+                           <div className="border-group hover:animate-fade-in"> <FaPhoneAlt style={{ color: 'rgba(14, 151, 123, 1)'}} className="h-5 w-5" /></div>
+                            <p className="font-normal ml-3 mt-1 hover:text-lg">+91 00000 00000</p>
                         </div>
-                        <div className=" flex justify-start items-start">
-                           <div className="border border-solid border-gray-500 p-2 rounded cursor-pointer"> <IoLocationSharp style={{ color: 'rgba(247, 144, 9, 1)'}} className="h-5 w-5"/></div>
-                            <p className="font-normal ml-3 mt-1">Somewhere in the World</p>
+                        <div className=" flex justify-start items-start cursor-pointer">
+                           <div className="border-group hover:animate-fade-in"> <IoLocationSharp style={{ color: 'rgba(247, 144, 9, 1)'}} className="h-5 w-5"/></div>
+                            <p className="font-normal ml-3 mt-1 hover:text-lg">Somewhere in the World</p>
                         </div>
                     </div>
                 </div>
