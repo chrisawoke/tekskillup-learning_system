3f1cbdbfa8a3f8619ffc18bf30d84a183fb3732c
Ifeoluwa <soyomboifeoluwa90@gmail.com>
2024-01-14 22:10:23 +0100

    contact-revamp

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Hero from "./Hero";
 import ContactSection from "./Section";
+import Image from "next/image";
 
 const Contact = () =>{
   return (
     <div className="">
       <Hero/>
+      <div className="md:flex justify-end items-center md:py-5 hidden md:w-[100%] h-[100%]">
+                <Image src="/assets/image/Frame 427320786.png" alt="Section"  width={400} height={300}/>
+      </div>
       <ContactSection/>
     </div>
   )
