c8355fbfbee17ab42b6cdaac0d3bfc41255ab933
Chris Awoke <uchechristian27@gmail.com>
2024-01-15 11:27:28 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,13 +1,8 @@
-import Header from "@/components/Header";
-import Hero from "@/components/Hero/Hero";
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Header/>
-      <Hero/>
+      <h1>Home Page ongoing</h1>
     </main>
   );
   return <main className=""></main>;
-
 }
