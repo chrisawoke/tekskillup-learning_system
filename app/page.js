16da572c68fd6e7db2962aea1ba3b64237be2cf1
Chris Awoke <uchechristian27@gmail.com>
2024-01-15 11:28:56 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Home Page ongoing</h1>
+      <h1>Work currently ongoing on the Home Page </h1>
     </main>
   );
   return <main className=""></main>;
