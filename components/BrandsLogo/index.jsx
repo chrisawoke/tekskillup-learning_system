a76c7e607e6ae642ca4c51e17f3952fcc8fa39ac
Elijah Ndenwa <elijahndenwa19@gmail.com>
2024-01-16 20:52:11 +0100

    Edited Clients Stats Section of the Home page

--- components/BrandsLogo/index.jsx
+++ components/BrandsLogo/index.jsx
@@ -3,10 +3,10 @@
 const BrandsLogo = () => {
   return (
     <>
-      {/* <!-- ===== Clients Start ===== --> */}
+      {/* <!-- ===== BrandsLogo Start ===== --> */}
       <section class="bg-teal-800 bg-opacity-5  dark:bg-gray-900">
     <div class="max-w-screen-xl px-4 pb-8 mx-auto lg:pb-16">
-        <div class="grid grid-cols-2 gap-8 text-gray-500 sm:gap-12 sm:grid-cols-3 lg:grid-cols-6 dark:text-gray-400">
+        <div class="grid grid-cols-2 gap-8 mt-10 text-gray-500 sm:gap-12 sm:grid-cols-3 lg:grid-cols-6 dark:text-gray-400">
             <a class="flex items-center lg:justify-center" target="_blank">
                 <svg class="h-9 hover:text-gray-900 dark:hover:text-white" viewBox="0 0 125 35" fill="currentColor"
                     xmlns="http://www.w3.org/2000/svg">
@@ -135,7 +135,7 @@ const BrandsLogo = () => {
         </div>
     </div>
 </section>
-      {/* <!-- ===== Clients End ===== --> */}
+      {/* <!-- ===== BrandsLogo End ===== --> */}
     </>
   );
 };
--- components/ServiceStats/index.jsx
+++ components/ServiceStats/index.jsx
@@ -1,41 +1,32 @@
 import React from "react";
 
 const ServiceStats = () => {
-  return <div class="max-w-7xl mx-auto  px-0 sm:px-6 lg:py-24 lg:px-0">
-    <h2 class="text-3xl font-extrabold text-center text-gray-900 sm:text-4xl">Our Success</h2>
-    <p class="text-center text-black text-opacity-80 text-xl font-normal font-['Open Sans'] leading-loose">Ornare id fames interdum porttitor nulla turpis etiam. Diam vitae sollicitudin at nec nam et pharetra gravida. Adipiscing a quis ultrices eu ornare tristique vel nisl orci. </p>
-    <div class="grid grid-cols-1 gap-5 sm:grid-cols-4 mt-4">
-        <div class="bg-white overflow-hidden sm:rounded-lg">
-            <div class="px-4 py-5 sm:p-6">
-                <h1 class="text-teal-600 text-8xl font-semibold pb-10 pr-10 font-['Open Sans'] leading-10">15K+</h1>
-                    <p class="text-slate-950 text-opacity-80 text-xl font-normal font-['Open Sans'] leading-relaxed">Students</p>
+  return <div class="max-w-4xl mx-auto  px-0 sm:px-6 lg:py-24 lg:px-0">
+          <h2 class="text-2xl mt-10 font-extrabold text-center text-gray-900 sm:text-4xl">Our Success</h2>
+         <p class="text-center text-black text-opacity-80 text-xl font-normal font-['Open Sans'] leading-loose">Ornare id fames interdum porttitor nulla turpis etiam. Diam vitae sollicitudin at nec nam et pharetra gravida. Adipiscing a quis ultrices eu ornare tristique vel nisl orci. </p>
+         <div class="grid grid-cols-1 gap-6 text-gray-500 sm:gap-2 sm:grid-cols-1 lg:grid-cols-5 md:grid-cols-3 dark:text-gray-400">
+            <div class="px-[-10] py-5 sm:p-6">
+                <h1 class="text-teal-600 text-[50px] font-semibold pb-10 pr-10 font-['Open Sans'] leading-10">15K+</h1>
+                    <p class="text-slate-950 mt-5  text-opacity-80 text-xl font-normal font-['Open Sans'] leading-relaxed">Students</p>
                 
             </div>
-        </div>
-        <div class="bg-white overflow-hidden sm:rounded-lg">
             <div class="px-4 py-5 sm:p-6">
-                <h1 class="text-teal-600 text-8xl font-semibold pb-10 pr-10 font-['Open Sans'] leading-10">35</h1>
-                    <p class="text-slate-950 text-opacity-80 text-xl font-normal font-['Open Sans'] leading-relaxed">Main questions</p>
+                <h1 class="text-teal-600 text-[50px] font-semibold pb-10 pr-10 font-['Open Sans'] leading-10">35</h1>
+                    <p class="text-slate-950 text-xl font-normal font-['Open Sans'] leading-relaxed">Main questions</p>
             </div>
-        </div>
-        <div class="bg-white overflow-hidden sm:rounded-lg">
             <div class="px-4 py-5 sm:p-6">
-                <h1 class="text-teal-600 text-8xl font-semibold pb-10 pr-10 font-['Open Sans'] leading-10">75 %</h1>
+                <h1 class="text-teal-600 text-[50px] font-semibold pb-10 pr-10 font-['Open Sans'] leading-10">75%</h1>
                     <p class="text-slate-950 text-opacity-80 text-xl font-normal font-['Open Sans'] leading-relaxed">Total success</p>
             </div>
-        </div>
-        <div class="bg-white overflow-hidden sm:rounded-lg">
+        
             <div class="px-4 py-5 sm:p-6">
-                <h1 class="text-teal-600 text-8xl font-semibold pb-10 pr-10 font-['Open Sans'] leading-10">16</h1>
+                <h1 class="text-teal-600 text-[50px] font-semibold pb-10 pr-10 font-['Open Sans'] leading-10">16</h1>
                     <p class="text-slate-950 text-opacity-80 text-xl font-normal font-['Open Sans'] leading-relaxed">Years of experience</p>
             </div>
-        </div>
-        <div class="bg-white overflow-hidden sm:rounded-lg">
             <div class="px-4 py-5 sm:p-6">
-                <h1 class="text-teal-600 text-8xl font-semibold pb-10 pr-10 font-['Open Sans'] leading-10">26</h1>
+                <h1 class="text-teal-600 text-[50px] font-semibold pb-10 pr-10 font-['Open Sans'] leading-10">26</h1>
                     <p class="text-slate-950 text-opacity-80 text-xl font-normal font-['Open Sans'] leading-relaxed">Chief experts</p>
             </div>
-        </div>
     </div>
 </div>;
 };
